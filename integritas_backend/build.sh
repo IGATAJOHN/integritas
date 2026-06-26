f5f7db4bcbb95b51a6cafd02b30c1ff4fca94457
IGATAJOHN <igatusjohn15@gmail.com>
2026-06-26 11:43:18 +0100

    Automatically create admin superuser account during Render build step

--- integritas_backend/build.sh
+++ integritas_backend/build.sh
@@ -6,3 +6,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create the admin user if it does not exist during deployment build
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@giaftechnology.com').exists() or User.objects.create_superuser('admin_giaf', 'admin@giaftechnology.com', 'Password123')" | python manage.py shell
+
