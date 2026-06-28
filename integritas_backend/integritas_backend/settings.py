d7b9ee73dbd7c8e268d5191fd65392b090033321
IGATAJOHN <igatusjohn15@gmail.com>
2026-06-28 21:53:41 +0100

    feat: add SMTP email configurations with console fallback during development

--- integritas_backend/integritas_backend/settings.py
+++ integritas_backend/integritas_backend/settings.py
@@ -127,3 +127,17 @@ CORS_ALLOW_CREDENTIALS = True
 # Disable automatic slash redirection for REST API endpoints
 APPEND_SLASH = False
 
+# Email Configuration
+if DEBUG and not os.getenv('EMAIL_HOST_USER'):
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+
+
