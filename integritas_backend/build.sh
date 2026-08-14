155e23c836edbf77a817bb2154a120799abdd883
IGATAJOHN <igatusjohn15@gmail.com>
2026-08-14 15:27:11 +0100

    update build script to create admin user with fiscaltransparency email on deploy

--- integritas_backend/build.sh
+++ integritas_backend/build.sh
@@ -8,5 +8,5 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Automatically create the admin user if it does not exist during deployment build
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@giaftechnology.com').exists() or User.objects.create_superuser('admin_giaf', 'admin@giaftechnology.com', 'Password123')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='mail@fiscaltransparency.org').exists() or User.objects.create_superuser('admin_fiscal', 'mail@fiscaltransparency.org', 'Integritas@7$')" | python manage.py shell
 
